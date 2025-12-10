5bf539caf829fe2067d6179a2b222b45036b763d
HG2505 <tranlegiahuy25052005@gmail.com>
2025-12-10 15:41:23 +0700

    thêm chức năng search

--- app/src/main/java/com/example/giuaki/HomeActivity.kt
+++ app/src/main/java/com/example/giuaki/HomeActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,13 +14,17 @@ class HomeActivity : AppCompatActivity() {
 
         val imgCart = findViewById<ImageView>(R.id.imgCart)
         val imgCategory = findViewById<ImageView>(R.id.imgCategories)
+        val search = findViewById<TextView>(R.id.tvSearch)
         imgCart.setOnClickListener {
-
             val intent = Intent(this, CartActivity::class.java)
             startActivity(intent)
         }
         imgCategory.setOnClickListener {
             val intent = Intent(this, CategoryActivity::class.java)
+            startActivity(intent)
+        }
+        search.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
             startActivity(intent)
         }
     }
